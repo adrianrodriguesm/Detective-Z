771dee66325f07e1ad44c126e7ef9eaa072bb697
Adrian <adrianrodriguesm98@gmail.com>
2021-04-10 22:00:17 +0100

    Player added when simulation finish.

--- Assets/Scripts/Gameplay/GameplayManager.cs
+++ Assets/Scripts/Gameplay/GameplayManager.cs
@@ -7,10 +7,29 @@ public class GameplayManager : Singleton<GameplayManager>
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
-
+    public GameObject player;
+    CameraController2D cameraController;
+    UnityTemplateProjects.SimpleCameraController simpleCamera;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        player.SetActive(false);
+        cameraController = Camera.main.GetComponent<CameraController2D>();
+        simpleCamera = Camera.main.GetComponent<UnityTemplateProjects.SimpleCameraController>();
+        cameraController.enabled = false;
     }
+
+    private void Update()
+    {
+        if(StoryManager.Instance.IsSimulationEnd())
+        {
+            simpleCamera.enabled = false;
+            cameraController.enabled = true;
+            player.SetActive(true);
+        }
+    }
+
+
+
 }
