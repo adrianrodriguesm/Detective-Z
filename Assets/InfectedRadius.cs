761b8afb87f38e88cf36fa9bbaf67f625f775a45
Adrian <adrianrodriguesm98@gmail.com>
2021-04-23 02:38:03 +0100

    Fixed infected agent bug.

---
+++ Assets/InfectedRadius.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfectedRadius : MonoBehaviour
+{
+    InfectedAgent infected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        infected = StoryManager.Instance.Infected;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Agent") && !(infected.Action is AttackAgent))
+        {
+            Debug.Log("DETECTED AGENT!");
+            infected.Action = new AttackAgent(infected, collision.GetComponent<AIAgent>());
+        }
+    }
+}
